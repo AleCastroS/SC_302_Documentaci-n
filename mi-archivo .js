cac3b7458adb0c55a3a069fcc548cbe1fcd020db
AleCastroS <145409666+AleCastroS@users.noreply.github.com>
2023-09-18 20:28:09 -0600

    Update mi-archivo .js

--- mi-archivo .js
+++ mi-archivo .js
@@ -1,3 +1,4 @@
 alert("Hola Mundo Editado");
 console.log("cambios en el archivo");
 #https://gist.github.com/AleCastroS/38261ede7da89bbe9924aa20c210e65d
+#https://github.com/AleCastroS/SC_302_Documentacion/projects?query=is%3Aopen
